4f408db770b957b9cceae6fed1e111f688fb6932
Mosiva <portret008@gmail.com>
2024-03-06 17:04:16 +0500

    style input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,13 @@ import PrimaryButton from "../components/PrimaryButton";
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -19,10 +25,20 @@ const styles = StyleSheet.create({
     marginTop: 100,
     backgroundColor: "#72063c",
     borderRadius: 8,
-    shadowColor: 'black',
-    shadowOffset: {width:0, height: 2},
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
-
- },
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 });
